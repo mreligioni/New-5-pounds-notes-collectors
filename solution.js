896fee6604ef83d2a43c022d50f25797e0f1d7d4
Mateusz <87975831+mreligioni@users.noreply.github.com>
2021-11-01 20:56:01 +0100

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,10 +1,12 @@
 function getNewNotes(salary,bills){
-    let sum = 0;
-    for (let i = 0; i < bills.length; i++){
-      sum += bills[i];
-    }
-    let disposableIncome = salary - sum;
-    let exactNumber = disposableIncome / 5;
-    let poundsNumber = Math.floor(exactNumber);
-    return poundsNumber;
+  let sum = 0;
+  for (let i = 0; i < bills.length; i++){
+    sum += bills[i];
   }
+  let disposableIncome = salary - sum;
+  let exactNumber = disposableIncome / 5;
+  let poundsNumber = Math.floor(exactNumber);
+    if (exactNumber < 0)
+      return 0;
+  return poundsNumber;
+}
